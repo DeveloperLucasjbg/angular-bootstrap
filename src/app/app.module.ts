cdf3460b34e14aea5d48c7e5f339a99e6366454a
DeveloperLucasjbg <76766269+DeveloperLucasjbg@users.noreply.github.com>
2022-04-06 18:55:47 -0300

    btn scroll top sin script - solo se ve

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { FeaturesComponent } from './features/features.component';
+import { LastNewsComponent } from './last-news/last-news.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    FeaturesComponent,
+    LastNewsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
